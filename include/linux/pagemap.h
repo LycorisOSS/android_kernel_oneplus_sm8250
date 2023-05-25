25c4d08fa392bc03d99410e979fa9ddff0071791
log1cs <tuan73176@gmail.com>
2023-05-25 07:21:11 +0700

    include: pagemap: Fix non-static declaration

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -688,51 +688,6 @@ void readahead_expand(struct readahead_control *ractl,
 		      loff_t new_start, size_t new_len);
 
 /**
- * page_cache_sync_readahead - generic file readahead
- * @mapping: address_space which holds the pagecache and I/O vectors
- * @ra: file_ra_state which holds the readahead state
- * @file: Used by the filesystem for authentication.
- * @index: Index of first page to be read.
- * @req_count: Total number of pages being read by the caller.
- *
- * page_cache_sync_readahead() should be called when a cache miss happened:
- * it will submit the read.  The readahead logic may decide to piggyback more
- * pages onto the read request if access patterns suggest it will improve
- * performance.
- */
-static inline
-void page_cache_sync_readahead(struct address_space *mapping,
-		struct file_ra_state *ra, struct file *file, pgoff_t index,
-		unsigned long req_count)
-{
-	DEFINE_READAHEAD(ractl, file, ra, mapping, index);
-	page_cache_sync_ra(&ractl, req_count);
-}
-
-/**
- * page_cache_async_readahead - file readahead for marked pages
- * @mapping: address_space which holds the pagecache and I/O vectors
- * @ra: file_ra_state which holds the readahead state
- * @file: Used by the filesystem for authentication.
- * @page: The page at @index which triggered the readahead call.
- * @index: Index of first page to be read.
- * @req_count: Total number of pages being read by the caller.
- *
- * page_cache_async_readahead() should be called when a page is used which
- * is marked as PageReadahead; this is a marker to suggest that the application
- * has used up enough of the readahead window that we should start pulling in
- * more pages.
- */
-static inline
-void page_cache_async_readahead(struct address_space *mapping,
-		struct file_ra_state *ra, struct file *file,
-		struct page *page, pgoff_t index, unsigned long req_count)
-{
-	DEFINE_READAHEAD(ractl, file, ra, mapping, index);
-	page_cache_async_ra(&ractl, page, req_count);
-}
-
-/**
  * readahead_page - Get the next page to read.
  * @rac: The current readahead request.
  *
